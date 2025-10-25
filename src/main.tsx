79c50cc8750a76dfc5046625edcf7fedbd1fb980
tonmoy <tonmoy3673@gmail.com>
2025-10-25 17:55:03 +0600

    :turck: set routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css';
+import RootLayout from "./layouts/RootLayout";
+import Home from "./components/Home";
 
 const root = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<RootLayout/>}>
+      <Route index element={<Home/>}/>
+      </Route>
     </Routes>
   </BrowserRouter>,
 );
