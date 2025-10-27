41ca641fd0af855a8581e72b291bd9cea153ba31
tonmoy <tonmoy3673@gmail.com>
2025-10-27 22:53:21 +0600

    :bulb: implment productContext

--- src/context/ProductContext.tsx
+++ src/context/ProductContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, type ReactNode } from "react";
+import { createContext, useContext, useState, type ReactNode } from "react";
 interface FilterContextType {
   searchQuery: string;
   setSearchQuery: (query: string) => void;
@@ -10,21 +10,45 @@ interface FilterContextType {
   setMaxPrice: (price: number | undefined) => void;
   keyword: string;
   setKeyword: (keyword: string) => void;
-};
+}
 
-interface ProductContextProviderProps{
-  children : ReactNode;
+interface ProductContextProviderProps {
+  children: ReactNode;
 }
 
 const ProductContext = createContext<FilterContextType | undefined>(undefined);
 
-export const ProductContextProvider:React.FC<ProductContextProviderProps> = ({children})=>{
-
-
-  return(
-    <ProductContext.Provider value={}>
+export const ProductContextProvider: React.FC<ProductContextProviderProps> = ({
+  children,
+}) => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
+  const [minPrice, setMinPrice] = useState<number | undefined>(undefined);
+  const [maxPrice, setMaxPrice] = useState<number | undefined>(undefined);
+  const [keyword, setKeyword] = useState<string>("");
+  return (
+    <ProductContext.Provider
+      value={{
+        searchQuery,
+        setSearchQuery,
+        selectedCategory,
+        setSelectedCategory,
+        minPrice,
+        setMinPrice,
+        maxPrice,
+        setMaxPrice,
+        keyword,
+        setKeyword,
+      }}
+    >
       {children}
     </ProductContext.Provider>
-  )
-}
+  );
+};
 
+
+export const useProductContext = ():FilterContextType =>{
+    const context =  useContext(ProductContext);
+    if (!context) throw new Error('No Product Context Found!');
+    return context;
+}
