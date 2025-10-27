94670723ea70081b3b64d860ba71186844373fd1
tonmoy <tonmoy3673@gmail.com>
2025-10-27 21:51:43 +0600

    :hammer: remove garbase

--- src/context/ProductContext.tsx
+++ src/context/ProductContext.tsx
@@ -1,35 +1,16 @@
-import { createContext, useState, type ReactNode } from "react";
 
 
-interface FilterContextType{
-    searchQuery : string;
-    setSearchQuery : (query : string)=>void;
-    selectedCategory : string;
-    setSelectedCategory : (category : string) => void;
-    minPrice : number | undefined;
-    setMinPrice : (price : number | undefined)=>void;
-    maxPrice : number | undefined;
-    setMaxPrice : (price : number | undefined) => void;
-    keyword : string;
-    setKeyword : (keyword:string)=>void;
-};
-
-interface ProductContextProvideProps{
-    children: ReactNode
+interface FilterContextType {
+  searchQuery: string;
+  setSearchQuery: (query: string) => void;
+  selectedCategory: string;
+  setSelectedCategory: (category: string) => void;
+  minPrice: number | undefined;
+  setMinPrice: (price: number | undefined) => void;
+  maxPrice: number | undefined;
+  setMaxPrice: (price: number | undefined) => void;
+  keyword: string;
+  setKeyword: (keyword: string) => void;
 }
 
-const ProductContext = createContext<FilterContextType | undefined>(undefined);
 
-export const ProductContextProvide : React.FC<ProductContextProvideProps> = ({children})=>{
-
-    const [searchQuery,setSearchQuery] = useState<string>('');
-    const [selectedCategory,setSelectedCategory] = useState<string>('');
-    const [minPrice,setMinPrice] = useState<number | undefined>();
-    const [maxPrice,setMaxPrice] = useState<number | undefined>();
-    const [keyword,setKeyword] = useState<string>('');
-    return(
-        <ProductContext.Provider value={{}}>
-            {children}
-        </ProductContext.Provider>
-    )
-}
