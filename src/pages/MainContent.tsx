8f577e6e96194e056e5a8bd8806ae9afab479b3b
tonmoy <tonmoy3673@gmail.com>
2025-10-30 21:56:27 +0600

    :books: set states

--- src/pages/MainContent.tsx
+++ src/pages/MainContent.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import { useProductContext } from "../context/ProductContext";
 
 const MainContent =()=>{
     const {searchQuery,selectedCategory,minPrice,maxPrice,keyword} = useProductContext();
+    const [products,setProducts] = useState<any[]>([]);
+    const [filter,setFilter] = useState<string>('');
+    const [currentPage,setCurrentPage] = useState<number>(1);
+    const [dropdownOpen,setDropdownOpen] = useState<boolean>(false);
+    const itemsPerPage: number =12;
+
     return (
         <div>MainContent</div>
     )
