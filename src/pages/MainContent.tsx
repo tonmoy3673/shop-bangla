a7c476181da44f2e36c3500400f71c53391c7c2e
tonmoy <tonmoy3673@gmail.com>
2025-10-31 15:04:11 +0600

    :art: set layouts

--- src/pages/MainContent.tsx
+++ src/pages/MainContent.tsx
@@ -10,8 +10,17 @@ const MainContent =()=>{
     const itemsPerPage: number =12;
 
     return (
-        <section className=" lg:w-220 sm:w-160 xs:w-[20rem] p-5">MainContent</section>
-    )
+        <section className=" lg:w-220 sm:w-160 xs:w-[20rem] p-5">MainContent
+        <div className="mb-5">
+            <div className="flex flex-col sm:flex-row justify-between items-center">
+                <div className="relative mb-5 mt-5">
+                    <button className="px-4 py-2 rounded-full flex items-center border"></button>
+                </div>
+            </div>
+        </div>
+        
+        </section>
+    ) 
 }
 
 export default MainContent;
