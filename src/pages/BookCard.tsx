fd80c35ac98f3a6098f31bff211dc01d469d8184
tonmoy <tonmoy3673@gmail.com>
2025-11-03 22:42:02 +0600

    :books: send products props to bookCard

--- src/pages/BookCard.tsx
+++ src/pages/BookCard.tsx
@@ -1,4 +1,11 @@
-const BookCard = ({product}) => {
+import type { ProductType } from "../types/products.types";
+
+interface BookCardProps {
+  product: ProductType;
+}
+const BookCard = ({ product }: BookCardProps) => {
+  
+  console.log("Filtered Products", product);
   return <div>Book Card</div>;
 };
 
