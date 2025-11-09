3b8d3ff31e9f6ed1c849b51d20d11789a0dae551
tonmoy <tonmoy3673@gmail.com>
2025-11-09 23:09:53 +0600

    :zap: fixed return value

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react";
 
 export function useDebounce<T> (value:T,delay:number):T{
-    const [debounce,setDebounce] = useState<T>(value);
+    const [debouncedValue,setDebounceValue] = useState<T>(value);
 
     useEffect(()=>{
         const handler = setTimeout(()=>{
-            setDebounce(value)
+            setDebounceValue(value)
         },delay);
 // ======== cleanup ============//
     return()=> clearTimeout(handler)
     },[value,delay]);
     
-    
+    // ========== return debounce ==========//
+    return debouncedValue;
 }
