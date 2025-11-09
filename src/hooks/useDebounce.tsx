5d4fe2e8c568c44a427760e3f1ab112c8c2fe53e
tonmoy <tonmoy3673@gmail.com>
2025-11-09 22:32:11 +0600

    :bulb: set debounce

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -1,3 +1,12 @@
-export function useDebounce (){
+import { useEffect, useState } from "react";
+
+export function useDebounce<T> (value:T,delay:number):T{
+    const [debounce,setDebounce] = useState<T>(value);
+
+    useEffect(()=>{
+        const handler = setTimeout(()=>{
+            setDebounce(value)
+        },delay)
+    },[value,delay]);
     
 }
